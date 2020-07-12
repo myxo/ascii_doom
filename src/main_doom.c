ce311b21b1d8cb9b64f2f5d719f45e6c0541221d
Egor <egorka99966@gmail.com>
2020-07-12 20:47:57 +0700

    Basic graphics with sample map. Can rotate on arrows

--- src/main_doom.c
+++ src/main_doom.c
@@ -1,8 +1,15 @@
+#define _USE_MATH_DEFINES
+
 #include "lib.h"
 
 #include "world_object.h"
 
+#include <windows.h>
+
 #include <stdio.h>
+
+#include <math.h>
+
 
 int width = 200;
 int height = 150;
@@ -10,13 +17,65 @@ int glyph_size = 8;
 
 int stop = 0;
 
+char map[8][8] = {
+	{"########"},
+	{"#  #   #"},
+	{"#      #"},
+	{"#  #   #"},
+	{"#  #   #"},
+	{"#  #   #"},
+	{"#  #   #"},
+	{"########"}
+};
+
+double player_x = 2;
+double player_y = 6;
+double player_angle = 0;
+double angle_of_vision = M_PI_4;
+
+void draw_screen() {
+	double d_angle = angle_of_vision / width;
+	double ray_angle = player_angle - angle_of_vision / 2;
+	double x = player_x;
+	double y = player_y;
+	double distance = 0;
+	double d_distance = 0.01;
+	int row = 0;
+	for (; ray_angle < player_angle + angle_of_vision / 2; ray_angle += d_angle) {
+		x = player_x;
+		y = player_y;
+		distance = 0;
+		while (map[(int)x][(int)y] != '#') {
+			x += d_distance * sin(ray_angle);
+			y += d_distance * cos(ray_angle);
+			distance += d_distance;
+		}
+		int num_of_wall_sym = height * (1 / distance);
+		if (num_of_wall_sym > height)
+			num_of_wall_sym = height;
+		for (int i = 0; i < num_of_wall_sym; i++) {
+			olc_draw(row, (height - num_of_wall_sym) / 2 + i, '#', FG_RED);
+		}
+		for (int i = 0; i < (height - num_of_wall_sym) / 2 + 1; i++) {
+			olc_draw(row, (height + num_of_wall_sym) / 2 + i, '*', FG_YELLOW);
+		}
+		row++;
+	}
+}
+
 int create() {
 	return 1;
 }
 
 void handle_input(float time_elapsed) {
-	if (olc_get_key(0x1B).held) { // Esc
+	if (olc_get_key(VK_ESCAPE).held) { // Esc
 		stop = 1;
+	}
+	if (olc_get_key(VK_LEFT).held) { // Esc
+		player_angle -= 0.01;
+	}
+	if (olc_get_key(VK_RIGHT).held) { // Esc
+		player_angle += 0.01;
 	}
 }
 
@@ -25,9 +84,11 @@ int update(float time_elapsed) {
 	if (stop) {
 		return 0;
 	}
+	olc_fill(0, 0, width, height, ' ', BG_BLACK);
 
-	const char * hello_str = "Hello world";
-	olc_draw_string(width / 2 - strlen(hello_str) / 2, height / 2, hello_str, FG_WHITE);
+	draw_screen();
+	//const char * hello_str = "Hello world";
+	//olc_draw_string(width / 2 - strlen(hello_str) / 2, height / 2, hello_str, FG_WHITE);
 
 	return 1;
 }
