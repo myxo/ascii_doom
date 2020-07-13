980550b058ca0895137ccd632aa65c1156fd1e9d
Egor <egorka99966@gmail.com>
2020-07-13 14:39:34 +0700

    Added Walls gradient

--- src/rendering.c
+++ src/rendering.c
@@ -18,6 +18,8 @@ char map[8][8] = {
 };
 
 void draw_screen(world_t world, int width, int height) {
+	int threshold1 = 125;
+	int threshold2 = 140;
 	double d_angle = world.player_angle_of_vision / width;
 	double ray_angle = world.player_angle - world.player_angle_of_vision / 2;
 	double x = world.player_pos_x;
@@ -37,15 +39,20 @@ void draw_screen(world_t world, int width, int height) {
 		int num_of_wall_sym = height * (2 / (distance));
 		if (num_of_wall_sym > height)
 			num_of_wall_sym = height;
+		char sym = '#';
+		if (distance > 1.7)
+			sym = '"';
+		if (distance > 3)
+			sym = '.';
 		for (int i = 0; i < num_of_wall_sym; i++) {
-			olc_draw(row, (height - num_of_wall_sym) / 2 + i, '#', FG_WHITE);
+			olc_draw(row, (height - num_of_wall_sym) / 2 + i, sym, FG_WHITE);
 		}
 		for (int i = height / 2; i < height; i++) {
 			if (i >= (height + num_of_wall_sym) / 2) {
-				if (i < 125) {
+				if (i < threshold1) {
 					olc_draw(row, i, '-', FG_GREY);
 				}
-				else if (i < 140) {
+				else if (i < threshold2) {
 					olc_draw(row, i, 'x', FG_GREY);
 				}
 				else {
