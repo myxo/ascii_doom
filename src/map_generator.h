7943c92d77ce87f8b125b68b22040ddd4150118e
ruca905 <yarkivaev@mail.ru>
2020-07-17 15:03:51 +0700

    +map_generator.[ch]

---
+++ src/map_generator.h
@@ -0,0 +1,15 @@
+#ifndef MAP_GENERATOR_H_
+#define MAP_GENERATOR_H_
+
+#include "world_object.h"
+
+typedef struct {
+
+} configuration_space_t;
+
+typedef struct {
+    point_t* shape;
+
+} room_t;
+
+#endif 
