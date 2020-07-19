63093792295f668573c78268c3ebc481d3341fa7
ruca905 <yarkivaev@mail.ru>
2020-07-19 22:46:51 +0700

    fix bug with overflow logging

--- src/logging.c
+++ src/logging.c
@@ -5,7 +5,7 @@
 
 typedef struct {
     double value;
-    char label[10];
+    char label[30];
 } var_to_display_t;
 
 typedef struct {
@@ -59,7 +59,11 @@ void add_watch(const char* label, double value) {
         increase_arr_vars_capacity(&display_logging);
     }
     display_logging.array[display_logging.len].value = value;
-    strncpy(display_logging.array[display_logging.len].label, label, 10);
+    if (strlen(label) >= 30) {
+        strcpy(display_logging.array[display_logging.len].label, "Name is too big(<30)");
+    } else {
+        strncpy(display_logging.array[display_logging.len].label, label, 30);
+    }
     display_logging.len++;
 }
 
@@ -83,8 +87,8 @@ void stop_watch(const char* label) {
 void display_watch() {
     if (display_logging.len == 0)
         return;
-    char str[27];
-    olc_fill(0, 0, 27, display_logging.len + 1, ' ', BG_BLACK);
+    char str[47];
+    olc_fill(0, 0, 47, display_logging.len + 1, ' ', BG_BLACK);
     for (int i = 0; i < display_logging.len; i++) {
         sprintf(str, "%s : %9.5f", display_logging.array[i].label, display_logging.array[i].value);
         olc_draw_string(0, i, str, FG_WHITE);
